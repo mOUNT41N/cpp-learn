87aa61a4776552f2032e25a7b43a821168b61e56
mOUNT41N <lianweimingchester@126.com>
2023-02-27 16:56:26 +0800

    update

--- 4/4-4-1.cpp
+++ 4/4-4-1.cpp
@@ -11,9 +11,22 @@ private:
 public:
     string m_SittingRoom;
 
+    Building()
+    {
+        this->m_SittingRoom = "客厅";
+        this->m_BedRoom = "卧室";
+    }
+
     friend void goodFriend(Building &building)
     {
         cout << "好基友正在访问： " << building.m_SittingRoom << endl;
         cout << "好基友正在访问： " << building.m_BedRoom << endl;
     }
 };
+
+int main()
+{
+    Building b;
+    goodFriend(b);
+    return 0;
+}
