a1ccdc197e231a6a0f0f500109ec26f2c4af788a
mOUNT41N <92198368+mOUNT41N@users.noreply.github.com>
2023-03-14 17:18:04 +0800

    update
    Signed-off-by: mOUNT41N <92198368+mOUNT41N@users.noreply.github.com>

--- worker_mangement_system/worker_manager.cpp
+++ worker_mangement_system/worker_manager.cpp
@@ -1,12 +1,86 @@
 #include "worker_manager.h"
 #include "worker.h"
+const int EMPLOYEE_DEPT_ID = 1;
+const int MANAGER_DEPT_ID = 2;
+const int BOSS_DEPT_ID = 3;
+
 WorkerManager::WorkerManager()
 {
+    this->m_employee_num = 0;      // 初始化人数
+    this->m_employee_array = NULL; // 初始化数组指针
 }
 WorkerManager::~WorkerManager()
 {
+    if (this->m_employee_array != nullptr)
+    {
+        delete[] this->m_employee_array;
+    }
 }
 
+inline void WorkerManager::AddEmployee()
+{
+    cout << "请输入增加职工数量:" << endl;
+    int add_num = 0;
+    cin >> add_num;
+    if (add_num > 0)
+    {
+        int new_size = this->m_employee_num + add_num;
+        Worker **new_space = new Worker *[new_size];
+        if (this->m_employee_array != nullptr)
+        {
+            for (int i = 0; i < this->m_employee_num; ++i)
+            {
+                new_space[i] = this->m_employee_array[i];
+            }
+        }
+
+        for (int i = 0; i < add_num; ++i)
+        {
+            int id;
+            string name;
+            int select;
+
+            cout << "请输入第 " << i + 1 << " 个新职工编号:" << endl;
+            cin >> id;
+
+            cout << "请输入第 " << i + 1 << " 个新职工姓名:" << endl;
+            cin >> name;
+
+            cout << "请选择该职工的岗位：" << endl;
+            cout << "1、普通职工" << endl;
+            cout << "2、经理" << endl;
+            cout << "3、老板" << endl;
+            cin >> select;
+
+            Worker *worker = NULL;
+
+            switch (select)
+            {
+            case EMPLOYEE_DEPT_ID: // 普通员工
+                worker = new Employee(id, EMPLOYEE_DEPT_ID, name);
+                break;
+            case MANAGER_DEPT_ID: // 经理
+                worker = new Manager(id, MANAGER_DEPT_ID, name);
+                break;
+            case BOSS_DEPT_ID: // 老板
+                worker = new Boss(id, BOSS_DEPT_ID, name);
+                break;
+            default:
+                break;
+            }
+
+            new_space[this->m_employee_num + i] = worker;
+        }
+        delete[] this->m_employee_array;    // 释放原有空间
+        this->m_employee_array = new_space; // 更改新空间的指向
+        this->m_employee_num = new_size;    // 更新新的个数
+        cout << "成功添加" << add_num << "名新职工！" << endl;
+    }
+    else
+    {
+        cout << "输入有误" << endl;
+    }
+}
 inline void WorkerManager::ShowMenu()
 {
     cout << "--------------------------------------------" << endl;
@@ -22,7 +96,6 @@ inline void WorkerManager::ShowMenu()
     cout << "--------------------------------------------" << endl;
     cout << endl;
 }
-
 inline void WorkerManager::ExitSys()
 {
     cout << "欢迎下次使用" << endl;
@@ -63,10 +136,3 @@ inline void WorkerManager::Select()
         ShowMenu();
     }
 }
-
-// int main()
-// {
-//     WorkerManager worker_manager;
-//     worker_manager.Select();
-//     return 0;
-// }
--- worker_mangement_system/worker_manager.h
+++ worker_mangement_system/worker_manager.h
@@ -6,12 +6,17 @@ using namespace std;
 class WorkerManager
 {
 public:
+    int m_employee_num;        // 记录文件中的人数个数
+    Worker **m_employee_array; // 员工数组的指针
+
+public:
     WorkerManager();  // 构造函数
     ~WorkerManager(); // 析构函数
 
-    void ShowMenu(); // 显示菜单
-    void Select();   // 选择菜单
-    void ExitSys();  // 退出
+    void ShowMenu();    // 显示菜单
+    void Select();      // 选择菜单
+    void ExitSys();     // 退出
+    void AddEmployee(); // 添加职工
 };
 
 #endif
