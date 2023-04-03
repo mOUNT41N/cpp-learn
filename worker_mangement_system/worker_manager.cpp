67a714bb3c0aedb1bcdde30608921dfdbb9285a5
mOUNT41N <92198368+mOUNT41N@users.noreply.github.com>
2023-04-03 13:10:26 +0800

    add find func
    Signed-off-by: mOUNT41N <92198368+mOUNT41N@users.noreply.github.com>

--- worker_mangement_system/worker_manager.cpp
+++ worker_mangement_system/worker_manager.cpp
@@ -101,6 +101,7 @@ void WorkerManager::Select()
             ModifyEmployee();
             break;
         case 5: // 查找职工
+            FindEmployee();
             break;
         case 6: // 排序职工
             break;
@@ -352,6 +353,71 @@ void WorkerManager::ModifyEmployee()
 
             // 保存到文件中
             this->SaveFile();
+        }
+    }
+}
+
+void WorkerManager::FindEmployee()
+{
+    if (!this->file_exist_flag)
+    {
+        cout << "文件不存在或记录为空！" << endl;
+    }
+    else
+    {
+        cout << "请输入查找的方式：" << endl;
+        cout << "1、按职工编号查找" << endl;
+        cout << "2、按姓名查找" << endl;
+
+        int select = 0;
+        cin >> select;
+
+        if (select == 1) // 按职工号查找
+        {
+            int id;
+            cout << "请输入查找的职工编号：" << endl;
+            cin >> id;
+
+            int ret = IsExist(id);
+            if (ret != -1)
+            {
+                cout << "查找成功！该职工信息如下：" << endl;
+                this->m_employee_array[ret]->showInfo();
+            }
+            else
+            {
+                cout << "查找失败，查无此人" << endl;
+            }
+        }
+        else if (select == 2) // 按姓名查找
+        {
+            string name;
+            cout << "请输入查找的姓名：" << endl;
+            cin >> name;
+
+            bool flag = false; // 查找到的标志
+            for (int i = 0; i < m_employee_num; i++)
+            {
+                if (m_employee_array[i]->m_name == name)
+                {
+                    cout << "查找成功,职工编号为："
+                         << m_employee_array[i]->m_id
+                         << " 号的信息如下：" << endl;
+
+                    flag = true;
+
+                    this->m_employee_array[i]->showInfo();
+                }
+            }
+            if (flag == false)
+            {
+                // 查无此人
+                cout << "查找失败，查无此人" << endl;
+            }
+        }
+        else
+        {
+            cout << "输入选项有误" << endl;
         }
     }
 }
