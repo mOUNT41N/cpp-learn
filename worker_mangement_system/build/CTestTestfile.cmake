1551547886de06181f2b05f911bbb229e880ecc3
mOUNT41N <92198368+mOUNT41N@users.noreply.github.com>
2023-04-03 13:36:43 +0800

    update
    Signed-off-by: mOUNT41N <92198368+mOUNT41N@users.noreply.github.com>

--- worker_mangement_system/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Code/c++/c++learn/worker_mangement_system
-# Build directory: D:/Code/c++/c++learn/worker_mangement_system/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
